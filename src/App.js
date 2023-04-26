734f9254d8db8ebf6f771ae7735b1bb1646bbb6c
zaursuleymnv <szaur018@mail.ru>
2023-04-26 17:11:28 +0200

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, {useEffect, useState} from 'react'
 
 function App() {
+
+  const [resourceType, setResourceType] = useState('posts')
+  const [items, setItems] = useState([])
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+
+  console.log('render');
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then(response => response.json())
+      .then(json => setItems(json))
+
+  }, [resourceType])
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth)
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-8">
+            <div className="btn-group mt-5" role="group">
+              <button onClick={() => setResourceType('posts')} type="button" className="btn btn-primary">Posts</button>
+              <button onClick={() => setResourceType('users')} type="button" className="btn btn-primary">Users</button>
+              <button onClick={() => setResourceType('comments')} type="button" className="btn btn-primary">Comments</button>
+            </div>
+            <h1 className='mb-3'>{resourceType}</h1>
+            {
+              items.map(item => {
+                return <pre>{JSON.stringify(item)}</pre>
+              })
+            }
+            <h1 className="window-width mt-5">
+              {windowWidth}
+            </h1>
+        </div>
+      </div>
     </div>
   );
 }
